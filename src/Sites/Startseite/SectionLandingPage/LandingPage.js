6d0f1a49811e803e29849df62edb57466e47e129
martinzimmerdesign <m.zimmer.business@gmail.com>
2020-04-16 18:51:57 +0200

    0.4.0

--- src/Sites/Startseite/SectionLandingPage/LandingPage.js
+++ src/Sites/Startseite/SectionLandingPage/LandingPage.js
@@ -8,6 +8,18 @@ import designJson from './img/design.json';
 import codeJson from './img/code.json';
 import shape1 from './img/blob-shape.svg';
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 export default class LandingPage extends React.Component {
   constructor() {
      super();
