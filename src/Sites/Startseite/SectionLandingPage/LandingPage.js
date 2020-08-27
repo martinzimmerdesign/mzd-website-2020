7502707126d8478e612210fdad3c58e1c3ca8985
martinzimmerdesign <m.zimmer.business@gmail.com>
2020-08-27 10:13:32 +0200

    0.4.9

--- src/Sites/Startseite/SectionLandingPage/LandingPage.js
+++ src/Sites/Startseite/SectionLandingPage/LandingPage.js
@@ -11,9 +11,9 @@ import logoJson from './img/logo.json';
 import shape1 from './img/blob-shape.svg';
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-// let vh = window.innerHeight * 0.01;
+let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 
 export default class LandingPage extends React.Component {
